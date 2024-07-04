381b3652c5267eaa5733cefb27b576570e73f9e5
Mateusz Majewski <mat.maj1992@gmail.com>
2024-07-04 15:18:19 +0200

    Seedowanie danych przy pierwszym zapytaniu

--- RestaurantApi/RestaurantApi/Program.cs
+++ RestaurantApi/RestaurantApi/Program.cs
@@ -1,4 +1,5 @@
 using RestaurantApi;
+using RestaurantApi.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,10 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<IWeatherForcastService, WeatherForcastService>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+
+seeder.Seed();
 
 app.UseHttpsRedirection();
 
