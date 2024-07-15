c8f1dd5d1372ab906a460b90e85774882b6f4a27
Mateusz Majewski <mat.maj1992@gmail.com>
2024-07-15 14:20:18 +0200

    Tworzenie nowej encji

--- RestaurantApi/RestaurantApi/Models/CreateRestaurantDto.cs
+++ RestaurantApi/RestaurantApi/Models/CreateRestaurantDto.cs
@@ -1,17 +1,16 @@
-﻿namespace RestaurantApi.Entities
+﻿namespace RestaurantApi.Models
 {
-    public class Restaurant
+    public class CreateRestaurantDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public bool HasDelivery { get; set; }
         public string ContactEmail { get; set; }
         public string? ContactNumber { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
 
-        public int AddressId { get; set; }
-        public virtual Address Address { get; set; }
-        public virtual List<Dish> Dishes { get; set; }
     }
 }
