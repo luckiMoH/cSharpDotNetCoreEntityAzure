d4923a249b671037cae652541533fa28245044d6
Mateusz Majewski <mat.maj1992@gmail.com>
2024-07-15 15:46:13 +0200

    Usuwanie zasobów - dodanie metody do serwisu oraz wywolanie jej w kontrolerze

--- RestaurantApi/RestaurantApi/Controllers/RestaurantController.cs
+++ RestaurantApi/RestaurantApi/Controllers/RestaurantController.cs
@@ -17,6 +17,21 @@ namespace RestaurantApi.Controllers
             _restaurantService = restaurantService;
         }
 
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _restaurantService.Delete(id);
+            
+            if (isDeleted)
+            {
+                return NoContent();
+            } 
+            else
+            {
+                return NotFound();
+            }
+        }
+
         [HttpPost]
         public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
         {
--- RestaurantApi/RestaurantApi/Services/IRestaurantService.cs
+++ RestaurantApi/RestaurantApi/Services/IRestaurantService.cs
@@ -7,5 +7,6 @@ namespace RestaurantApi.Services
         int Create(CreateRestaurantDto dto);
         IEnumerable<RestaurantDto> GetAll();
         RestaurantDto GetById(int id);
+        bool Delete(int id);
     }
 }
