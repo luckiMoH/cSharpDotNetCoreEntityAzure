375b0eb86d8243ef31b2b7381d48cfcdf1e1fcd6
Mateusz Majewski <mat.maj1992@gmail.com>
2024-07-15 15:16:22 +0200

    refaktor kontrorela - stworzenie serwisu oraz interfejsu dla kontrorela

--- RestaurantApi/RestaurantApi/Program.cs
+++ RestaurantApi/RestaurantApi/Program.cs
@@ -1,5 +1,6 @@
 using RestaurantApi;
 using RestaurantApi.Entities;
+using RestaurantApi.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 
 var app = builder.Build();
 
---
+++ RestaurantApi/RestaurantApi/Services/RestaurantService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Entities;
+using RestaurantApi.Models;
+
+namespace RestaurantApi.Services
+{
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly RestaurantDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public RestaurantDto GetById(int id)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null) return null;
+
+            var result = _mapper.Map<RestaurantDto>(restaurant);
+            return result;
+        }
+
+        public IEnumerable<RestaurantDto> GetAll()
+        {
+            var restaurants = _dbContext
+                    .Restaurants
+                    .Include(r => r.Address)
+                    .Include(r => r.Dishes)
+                    .ToList();
+
+            var restaurantsDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            return restaurantsDtos;
+        }
+
+        public int Create(CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+
+            return restaurant.Id;
+        }
+    }
+}
